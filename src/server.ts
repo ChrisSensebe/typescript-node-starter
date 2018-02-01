d5b0b24f6635e13e8bda9a83a08add81b2b87dd9
ChrisSensebe <christophe.sensebe@gmail.com>
2018-02-01 21:27:55 +0100

    adding dummy http server

--- src/server.ts
+++ src/server.ts
@@ -1,1 +1,5 @@
-console.log('trololo world');
+const http = require('http');
+const port = process.env.port || 8080;
+
+const server = http.createServer((req, res) => res.end('hello world'));
+server.listen(port, err => err ? console.log(`Oups, ${err}`) : console.log(`server listening on ${port}`));
